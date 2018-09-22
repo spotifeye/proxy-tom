58f3831316e2e2466932e52992f2615fc993d5a7
wissemgamra <wissem.gamra@yale.edu>
2018-09-22 11:09:29 -0700

    Final changes to proxy. This is a deployable version

--- server.js
+++ server.js
@@ -13,22 +13,22 @@ server.use(cors());
 
 // Albums & Player
 server.get('/artists/albums/:artistID', (req, res) => {
-  res.redirect('http://localhost:3001' + req.url);
+  res.redirect('http://52.15.129.193' + req.url);
 });
 
 // Related Artists
-server.get('/relatedArtists/id/artist', (req, res) => {
-  res.redirect('http://localhost:3002' + req.url);
+server.get('/artist/:id/relatedArtists', (req, res) => {
+  res.redirect('http://18.206.245.56' + req.url);
 });
 
 // Popular Songs
 server.get('/artist/:id', (req, res) => {
-  res.redirect('http://localhost:3003/' + req.url);
+  res.redirect('http://18.224.17.253' + req.url);
 });
 
 // Header
 server.get('/artists/:artistID', (req, res) => {
-   res.redirect('http://localhost:3004' + req.url);
+   res.redirect('http://35.172.133.115' + req.url);
 });
 
 server.listen(3000, console.log('Listening on:', 3000));
